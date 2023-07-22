0a57847f78203579959d1aeff4f36b8060a9a60f
swayammaheshwari <swayammaheshwari890@gmail.com>
2023-07-22 23:00:07 +0530

    portfolio updates

--- src/component/Portfolio-page/PortfolioPage.tsx
+++ src/component/Portfolio-page/PortfolioPage.tsx
@@ -1,54 +1,80 @@
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import styles from "./style.module.css";
+import { Typography } from "@mui/material";
 
 export default function PortfolioPage() {
   return (
     <>
       <Box m={2}>
+        <Typography variant="h2" align="center">Social Media</Typography>
+        <Box sx={{ display: "flex", justifyContent: "center", alignContent: "center" }}>
+          <Grid container spacing={1}>
+            <Grid item xs={12} sm={12} md={3}>
+              <div className={styles.img_div}>
+                <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={12} md={3}>
+              <div className={styles.img_div}>
+                <img src="portfolio_img/portfolio_2.png" alt="" className={styles.responsive_img} />
+              </div>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <Box m={2}>
+        <Typography variant="h2" align="center">Branding</Typography>
         <Grid container spacing={1}>
-          <Grid item xs={12} sm={12} md={6}>
+          <Grid item xs={12} sm={12} md={4}>
             <div className={styles.img_div}>
               <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
             </div>
           </Grid>
-          <Grid item xs={12} sm={12} md={6}>
+          <Grid item xs={12} sm={12} md={4}>
             <div className={styles.img_div}>
               <img src="portfolio_img/portfolio_2.png" alt="" className={styles.responsive_img} />
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <div className={styles.img_div}>
+              <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <div className={styles.img_div}>
+              <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <div className={styles.img_div}>
+              <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <div className={styles.img_div}>
+              <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
             </div>
           </Grid>
         </Grid>
       </Box>
       <Box m={2}>
+        <Typography variant="h2" align="center">Youtube</Typography>
         <Grid container spacing={1}>
           <Grid item xs={12} sm={12} md={4}>
-            <div className={styles.img_div}>
-              <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
+            <div className={styles.you_div}>
+              <iframe width="460" height="315" src="https://www.youtube.com/embed/ZlKnLyKO5UU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
             </div>
           </Grid>
           <Grid item xs={12} sm={12} md={4}>
-            <div className={styles.img_div}>
-              <img src="portfolio_img/portfolio_2.png" alt="" className={styles.responsive_img} />
+            <div className={styles.you_div}>
+              <iframe width="460" height="315" src="https://www.youtube.com/embed/ZlKnLyKO5UU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+
             </div>
           </Grid>
           <Grid item xs={12} sm={12} md={4}>
-            <div className={styles.img_div}>
-              <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
-            </div>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <div className={styles.img_div}>
-              <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
-            </div>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <div className={styles.img_div}>
-              <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
-            </div>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <div className={styles.img_div}>
-              <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
+            <div className={styles.you_div}>
+              <iframe width="460" height="315" src="https://www.youtube.com/embed/ZlKnLyKO5UU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
             </div>
           </Grid>
         </Grid>
