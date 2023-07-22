6fd8462ae8636e4cbe62a3b6e8836477ff121390
Akash Sharma <akashsha@lentra.ai>
2023-07-22 23:49:47 +0530

    bug fix for ui

--- src/component/Portfolio-page/PortfolioPage.tsx
+++ src/component/Portfolio-page/PortfolioPage.tsx
@@ -4,57 +4,65 @@ import styles from "./style.module.css";
 import { Typography } from "@mui/material";
 
 export default function PortfolioPage() {
+
+const isMobile = window.innerWidth < 600;
+
+
   return (
     <>
-      <Box m={2}>
+ { isMobile?    <Box sx={{maxWidth: "100%", m : "20px"}}>
         <Typography variant="h2" align="center">Social Media</Typography>
-        <Box sx={{ display: "flex", justifyContent: "center", alignContent: "center" }}>
-          <Grid container spacing={1}>
-            <Grid item xs={12} sm={12} md={3}>
-              <div className={styles.img_div}>
-                <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
-              </div>
-            </Grid>
-            <Grid item xs={12} sm={12} md={3}>
-              <div className={styles.img_div}>
-                <img src="portfolio_img/portfolio_2.png" alt="" className={styles.responsive_img} />
-              </div>
-            </Grid>
-          </Grid>
+        <Box sx={{ display: "flex", justifyContent: "center", alignContent: "center" , flexDirection : "column" }}>
+              <Box >
+                <img src="portfolio_img/portfolio_1.png" alt="" width={"100%"} />
+              </Box>
+              <Box>
+                <img src="portfolio_img/portfolio_2.png" alt="" width={"100%"}/>
+              </Box>
         </Box>
-      </Box>
-      <Box m={2}>
+      </Box> : <Box m={2} sx={{maxWidth: "100%"}}>
+        <Typography variant="h2" align="center">Social Media</Typography>
+        <Box sx={{ display: "flex", justifyContent: "center", alignContent: "center" , width: "100%" }}>
+              <Box >
+                <img src="portfolio_img/portfolio_1.png" alt="" width={"100%"} />
+              </Box>
+              <Box>
+                <img src="portfolio_img/portfolio_2.png" alt="" width={"100%"}/>
+              </Box>
+        </Box>
+      </Box>}
+      {/* <Box m={2}>
         <Typography variant="h2" align="center">Branding</Typography>
         <Grid container spacing={1}>
           <Grid item xs={12} sm={12} md={4}>
-            <div className={styles.img_div}>
+            <Box className={styles.img_Box}>
               <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
-            </div>
+            </Box>
           </Grid>
           <Grid item xs={12} sm={12} md={4}>
-            <div className={styles.img_div}>
+            <Box className={styles.img_Box}>
               <img src="portfolio_img/portfolio_2.png" alt="" className={styles.responsive_img} />
-            </div>
+            </Box>
           </Grid>
           <Grid item xs={12} sm={12} md={4}>
-            <div className={styles.img_div}>
+            <Box className={styles.img_Box}>
               <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
-            </div>
+            </Box>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-            <div className={styles.img_div}>
+            <Box className={styles.img_Box}>
               <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
-            </div>
+            </Box>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-            <div className={styles.img_div}>
+            <Box className={styles.img_Box}>
               <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
-            </div>
+            </Box>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-            <div className={styles.img_div}>
+            <Box className={styles.img_Box}>
               <img src="portfolio_img/portfolio_1.png" alt="" className={styles.responsive_img} />
-            </div>
+            </Box>
           </Grid>
         </Grid>
       </Box>
@@ -62,23 +70,23 @@ export default function PortfolioPage() {
         <Typography variant="h2" align="center">Youtube</Typography>
         <Grid container spacing={1}>
           <Grid item xs={12} sm={12} md={4}>
-            <div className={styles.you_div}>
+            <Box className={styles.you_Box}>
               <iframe width="460" height="315" src="https://www.youtube.com/embed/ZlKnLyKO5UU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
-            </div>
+            </Box>
           </Grid>
           <Grid item xs={12} sm={12} md={4}>
-            <div className={styles.you_div}>
+            <Box className={styles.you_Box}>
               <iframe width="460" height="315" src="https://www.youtube.com/embed/ZlKnLyKO5UU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
 
-            </div>
+            </Box>
           </Grid>
           <Grid item xs={12} sm={12} md={4}>
-            <div className={styles.you_div}>
+            <Box className={styles.you_Box}>
               <iframe width="460" height="315" src="https://www.youtube.com/embed/ZlKnLyKO5UU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
-            </div>
+            </Box>
           </Grid>
         </Grid>
-      </Box>
+      </Box> */}
 
     </>
   );
